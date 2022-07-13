a288db9e109000a8f5cea501e7d2404062f15aed
William Squires <william.squires@jetstack.io>
2022-07-13 15:43:03 +0100

    Change structs, use pointers, add comments, user marshal indent

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -10,43 +9,45 @@ import (
 	"encoding/json"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-type info struct {
-	instances []instance
+// Info is all the information displayed by the UI.
+type Info struct {
+	ClusterName string     `json:"clusterName"`
+	Instances   []Instance `json:"instances"`
 }
 
-func (i *info) AddItem(item instance) []instance {
-	i.instances = append(i.instances, item)
-	return i.instances
-  }
-
-type instance struct {
-	name string
-	capacity resources 
-	workloads []workload
+// Instance represents a cluster node.
+type Instance struct {
+	Name      string     `json:"name"`
+	Capacity  *Resources `json:"capacity"`
+	Workloads []Workload `json:"workloads"`
 }
 
-type resources struct {
-	memoryMi int64
-	cpuM int64
+// Resources represents CPU and memory in standardised units.
+type Resources struct {
+	MemoryMi int64 `json:"memoryMi"`
+	CpuM     int64 `json:"cpuM"`
 }
 
-type workload struct {
-	id string
-	name string
-	requests resources 
-	limits resources
+// Workload represents an instance of a running workload.
+type Workload struct {
+	Id       string     `json:"id"`
+	Name     string     `json:"name"`
+	Requests *Resources `json:"requests"`
+	Limits   *Resources `json:"limits"`
 }
 
 func main() {
+	// Default the Kube context and read as an argument if set.
 	contextName := ""
 	if len(os.Args) >= 2 {
 		contextName = os.Args[1]
 	}
 
+	// Create Kube client to access the cluster.
 	client, err := newClient(contextName)
 	if err != nil {
 		log.Fatal(err)
@@ -61,13 +62,14 @@ func main() {
 	// 	fmt.Println(pod.Name)
 	// }
 
+	// Get a list of nodes from the cluster.
 	nodes, err := client.CoreV1().Nodes().List(context.TODO(), meta_v1.ListOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	instanceList := make([]instance, 0)
-
+	// Create a list of instances to represent cluster nodes and the workloads running on them.
+	instanceList := make([]Instance, 0)
 	for _, node := range nodes.Items {
 		nodeName := node.Name
 		nodeMemory := node.Status.Capacity["memory"]
@@ -75,33 +77,28 @@ func main() {
 		nodeCpu := node.Status.Capacity["cpu"]
 		nodeCpuM := nodeCpu.MilliValue()
 
-
-		n := instance{
-			name: nodeName,
-			capacity: resources{
-				memoryMi: nodeMemoryMi,
-				cpuM: nodeCpuM,
+		inst := Instance{
+			Name: nodeName,
+			Capacity: &Resources{
+				MemoryMi: nodeMemoryMi,
+				CpuM:     nodeCpuM,
 			},
 		}
-		fmt.Println(node.Name)
 
-		instanceList = append(instanceList, n)
+		instanceList = append(instanceList, inst)
 	}
 
-	fmt.Println(instanceList)
-
-	i := info{
-		instances: instanceList,
+	i := &Info{
+		ClusterName: "test",
+		Instances:   instanceList,
 	}
 
-
-	b, err := json.Marshal(i)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(string(b))	
-
+	b, err := json.MarshalIndent(i, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(b))
 }
 
 func newClient(contextName string) (kubernetes.Interface, error) {
@@ -115,33 +112,3 @@ func newClient(contextName string) (kubernetes.Interface, error) {
 
 	return kubernetes.NewForConfig(config)
 }
-
-// [
-// 		{
-// 		  id: "nodepool-id",
-// 		  availableResources: {
-// 			memory: 3072,
-// 			cpu: 4000,
-// 		  },
-// 		  nodes: [
-// 			{
-// 			  id: "node-id",
-// 			  workloads: [
-// 				{
-// 				  id: "workload-id",
-// 				  name: "workload-name",
-// 				  allocatedResources: {
-// 					memory: 64,
-// 					cpu: 250,
-// 				  },
-// 				  resourcesLimits: {
-// 					memory: 128,
-// 					cpu: 500,
-// 				  },
-// 				},
-// 			  ],
-// 			},
-// 		  ],
-// 		},
-// 	  ]
-// }
