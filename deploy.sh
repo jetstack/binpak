c774e86fdec837a28e29369295878f2399d48cdc
Becky Pauley <beckypauley@WKSMAC151443.local>
2022-07-14 13:20:34 +0100

    update deployment logica, add manifests

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,5 @@ docker push ${IMAGE}:${VERSION}
 gcloud container clusters get-credentials binpak --zone europe-west2-c --project jetstack-wil
 
 # Deploy to kubernetes
-kubectl create deployment binpak --image=${IMAGE}:${VERSION} --dry-run=client -o yaml > binpak-deployment.yaml
-kubectl apply -f binpak-deployment.yaml -n binpak
-
-# svc of type load balancer
-# TODO
+# kubectl create deployment binpak --image=${IMAGE}:${VERSION} --port=8080 --dry-run=client -o yaml > manifests/binpak-deployment.yaml
+kubectl apply -f manifests -n binpak
