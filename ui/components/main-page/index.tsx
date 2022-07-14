4f8e844786a12432dc8efae65816d6849f722ac8
Constantin Chirila <constantin.chirila@gmail.com>
2022-07-14 15:48:37 +0100

    And God commanded: Let there be MUSIC!!!

--- ui/components/main-page/index.tsx
+++ ui/components/main-page/index.tsx
@@ -1,14 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Toggle, Node } from "../";
 import styles from "./styles.module.scss";
-import { mockResponse } from "./constants";
+
+const env = process.env.NODE_ENV;
+
+const apiUrl = env == "production" ? "/api/info" : "http://35.246.94.55/info";
 
 export function MainPage() {
   const [isTypeOfResourceToggled, setIsTypeOfResourceToggled] = useState(false);
   const [isAllocated, setIsAllocated] = useState(false);
   const [hoveredWorkload, setHoveredWorkload] = useState(undefined);
+  const [soundOn, setSoundOn] = useState(false);
+  const [sound, setSound] = useState<any>(undefined);
+  const [response, setResponse] = useState<any>(undefined);
 
-  const response = mockResponse;
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const sound = new Audio("/tetris.mp3");
+      sound.volume = 0.1;
+      sound.loop = true;
+      setSound(sound);
+    }
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      const response = await fetch(apiUrl);
+      const json = await response.json();
+      setResponse(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function toggleSound() {
+    if (soundOn) {
+      sound.pause();
+    } else {
+      sound.play();
+    }
+    setSoundOn(!soundOn);
+  }
+
   const isCPU = isTypeOfResourceToggled;
 
   return (
@@ -16,6 +50,10 @@ export function MainPage() {
       <div className={styles.logo}>
         <img src="./images/binpak.svg" alt="" />
       </div>
+      <div
+        onClick={() => toggleSound(soundOn)}
+        className={`${styles.soundIcon} ${soundOn ? styles.soundOn : ""}`}
+      ></div>
       <div className={styles.content}>
         <p>
           This is an introduction text to explain what binpak is. This is an
@@ -40,20 +78,26 @@ export function MainPage() {
           onToggle={setIsAllocated}
         />
       </div>
-      <h3>Cluster name: {response.clusterName}</h3>
-      <div className={styles.list}>
-        {response.instances.map((instance: any) => (
-          <Node
-            key={instance.name}
-            workloads={instance.workloads}
-            limit={isCPU ? instance.capacity.cpuM : instance.capacity.memoryMi}
-            isCPU={isCPU}
-            isAllocated={isAllocated}
-            hoveredWorkload={hoveredWorkload}
-            setHoveredWorkload={setHoveredWorkload}
-          />
-        ))}
-      </div>
+      {response && (
+        <>
+          <h3>Cluster name: {response.clusterName}</h3>
+          <div className={styles.list}>
+            {response.instances.map((instance: any) => (
+              <Node
+                key={instance.name}
+                workloads={instance.workloads}
+                limit={
+                  isCPU ? instance.capacity.cpuM : instance.capacity.memoryMi
+                }
+                isCPU={isCPU}
+                isAllocated={isAllocated}
+                hoveredWorkload={hoveredWorkload}
+                setHoveredWorkload={setHoveredWorkload}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
