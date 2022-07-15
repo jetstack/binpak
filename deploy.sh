d03b9d685e26658e06dab7dcc8d68b4db2539c3f
Jon Tutcher <jon.tutcher@jetstack.io>
2022-07-15 17:48:37 +0100

    Add support for mock/demo static image; add license

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 # To run: run ./depoy.sh from the root of binpak repo
 BACKEND_IMAGE=europe-west1-docker.pkg.dev/jetstack-wil/binpak/binpak
 UI_IMAGE=europe-west1-docker.pkg.dev/jetstack-wil/binpak/binpak-ui
+COMBINED_IMAGE=europe-west1-docker.pkg.dev/jetstack-wil/binpak/binpak-combined
 
 # TODO: Proper versioning
 VERSION=latest
@@ -13,20 +14,25 @@ VERSION=latest
 echo "Building ${BACKEND_IMAGE}:${VERSION}"
 docker build -t ${BACKEND_IMAGE}:${VERSION} . --platform linux/amd64
 docker push ${BACKEND_IMAGE}:${VERSION}
-
-# Build and push UI
+#
+## Build and push UI
 echo "Building ${UI_IMAGE}:${VERSION}"
 docker build -t ${UI_IMAGE}:${VERSION} ./ui/. --platform linux/amd64
 docker push ${UI_IMAGE}:${VERSION}
 
+## Build and push Combined image for mock/demo purposes
+echo "Building ${COMBINED_IMAGE}:${VERSION}"
+docker build -t europe-west1-docker.pkg.dev/jetstack-wil/binpak/binpak-combined:latest -f ui/demo-image.Dockerfile ui/ --platform linux/amd64
+docker push ${COMBINED_IMAGE}:${VERSION}
+
 # Set kubernetes context
-gcloud container clusters get-credentials binpak --zone europe-west2-c --project jetstack-wil
-
-# Deploy to kubernetes
-BACKEND_IMAGE_TAG=${BACKEND_IMAGE}:${VERSION} envsubst < manifests/deployment-backend.tpl > \
-  manifests/deployment-backend.yaml
-UI_IMAGE_TAG=${UI_IMAGE}:${VERSION} envsubst < manifests/deployment-ui.tpl > \
-  manifests/deployment-ui.yaml
-
-# kubectl create deployment binpak --image=${IMAGE}:${VERSION} --port=8080 --dry-run=client -o yaml > manifests/deployment-backend.tpl
-kubectl apply -f manifests
+#gcloud container clusters get-credentials binpak --zone europe-west2-c --project jetstack-wil
+#
+## Deploy to kubernetes
+#BACKEND_IMAGE_TAG=${BACKEND_IMAGE}:${VERSION} envsubst < manifests/deployment-backend.tpl > \
+#  manifests/deployment-backend.yaml
+#UI_IMAGE_TAG=${UI_IMAGE}:${VERSION} envsubst < manifests/deployment-ui.tpl > \
+#  manifests/deployment-ui.yaml
+#
+## kubectl create deployment binpak --image=${IMAGE}:${VERSION} --port=8080 --dry-run=client -o yaml > manifests/deployment-backend.tpl
+#kubectl apply -f manifests
--- infrastructure/main.auto.tfvars
+++
@@ -1,4 +0,0 @@
-project = "jetstack-wil"
-region = "europe-west1"
-domain_name = "binpak.me"
-nginx_ingress_ip = "35.189.89.170"
